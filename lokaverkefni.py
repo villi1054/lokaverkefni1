6eb2d78c3dc752a75d7b34f976edc06ceefac9e1
SmariArnar <33835239+SmariArnar@users.noreply.github.com>
2017-11-21 11:32:30 +0000

    lausn við verkefni 3

--- lokaverkefni.py
+++ lokaverkefni.py
@@ -57,16 +57,75 @@ while val !=0:
         print()
         print("Takk fyrir að nota forritið mitt")
     elif val == 3:
-        print("Sláðu inn 10 orð og forritið finnur út allskonar um þau")
-        listi = []
-        lengd = 10
-        while len(listi) < lengd:
-            ord = input("Sláðu inn orð í listann þinn")
-            listi.append(ord)
-            listi4 = []
-            if len(ord) == 4:
-                listi4.insert(i, char)
-                print(listi4)
+        from random import randint
+# create a list of play options
+t = ["Steinn", "Blað", "Skæri"]
+# assign a random play to the computer
+computer = t[randint(0, 2)]
+# set player to False
+notandi_vann = 0
+notandi_vann += 1
+
+talva_vann = 0
+talva_vann += 1
+
+jafntefli = 0
+jafntefli += 1
+
+spilanir = 0
+spilanir += 1
+
+player = False
+print("Reglurnar:")
+print("Skæri vinnur blað")
+print("Steinn vinnur skæri")
+print("Blað vinnur steinn")
+print("'Hætta' til að hætta")
+print("Sláðu inn 'Skæri', 'Blað', eða 'Steinn' til að byrja ")
+nafn = input("Hvað heitir þú ?. ")
+aldur = int(input("Sláðu inn aldur þinn? "))
+while player == False:
+    # set player to True
+    spilanir += 1
+    player = input("Skæri, Blað, Steinn")
+    if player == computer:
+        print("Jafntefli!")
+        jafntefli += 1
+    elif player == "Steinn":
+        teljari2 = 0
+        teljari2 += 1
+        if computer == "Blað":
+            print("Þú tapar!", computer, "vinnur", player)
+            talva_vann += 1
+        else:
+            print("Þú vinnur!", player, "vinnur", computer)
+            notandi_vann += 1
+    elif player == "Blað":
+        if computer == "Skæri":
+            print("Þú tapar!", computer, "sker", player)
+            talva_vann += 1
+        else:
+            print("Þú vinnur!", player, "vinnur", computer)
+            notandi_vann += 1
+    elif player == "Skæri":
+        if computer == "Steinn!":
+            print("Þú tapar....", computer, "vinnur", player)
+            talva_vann += 1
+        else:
+            print("Þú vinnur!", player, "sker", computer)
+            notandi_vann += 1
+    elif player == "Hætta":
+        print("Þú vannst "+ str(notandi_vann) +" Sinnum.")
+        print("Talvan vann þig ", str(talva_vann), " sinni.")
+        print("Þið kom upp jafntefli", str(jafntefli), " sinnum.")
+        print("Leikurinn var spilaður", str(spilanir), " sinnum.")
+        print(str(nafn),",aldur", str(aldur))
+        break
+    else:
+        print("Villa í innslátti! Sláðu inn annaðhvort Skæri, Blað eða Steinn")
+    # player was set to True, but we want it to be False so the loop continues
+    player = False
+    computer = t[randint(0, 2)]
     elif val == 4:
         lengd1 = 7
         lengd2 = 6
